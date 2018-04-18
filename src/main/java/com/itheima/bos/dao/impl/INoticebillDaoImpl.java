76f896b72a93404be8d67b5cb3c63b1f737564ed
Mrwu565 <zhipaikkk@163.com>
2018-04-18 21:32:43 +0800

    加了实体

---
+++ src/main/java/com/itheima/bos/dao/impl/INoticebillDaoImpl.java
@@ -0,0 +1,16 @@
+package com.itheima.bos.dao.impl;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.hibernate.criterion.DetachedCriteria;
+
+import com.itheima.bos.dao.INoticebillDao;
+import com.itheima.bos.dao.base.impl.BaseDaoImpl;
+import com.itheima.bos.domain.Noticebill;
+import com.itheima.bos.utils.PageBean;
+
+public class INoticebillDaoImpl extends BaseDaoImpl<Noticebill> implements INoticebillDao {
+
+
+}
---
+++ src/main/java/com/itheima/bos/web/action/NoticebillAction.java
@@ -0,0 +1,38 @@
+package com.itheima.bos.web.action;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import com.itheima.bos.crm.Customer;
+import com.itheima.bos.crm.ICustomerService;
+import com.itheima.bos.domain.Noticebill;
+import com.itheima.bos.service.INoticebillService;
+import com.itheima.bos.web.action.base.BaseAction;
+@Controller
+@Scope("prototype")
+public class NoticebillAction extends BaseAction<Noticebill> {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    
+    @Autowired
+    private ICustomerService customerService;
+                  
+    public String findCustomerByTelephone() {
+        String telephone=model.getTelephone();
+        Customer findCustomerByTelephone = customerService.findCustomerByTelephone(telephone);
+        this.java2Json(findCustomerByTelephone, new String[] {});
+        return NONE;
+        }
+    @Autowired
+    private INoticebillService noticebillService;
+    
+    public String add() {
+        noticebillService.save(model);
+        
+        return "noticebill_add";}
+    
+}
